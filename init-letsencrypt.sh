781e947fb0084ac23ef281977aea80012f73141f
hyunul <jungwj1023@naver.com>
2025-12-18 23:56:41 +0900

    fix: docker 명령어 수정

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if ! [ -x "$(command -v docker-compose)" ]; then
-  echo 'Error: docker-compose is not installed.' >&2
+if ! [ -x "$(command -v docker compose)" ]; then
+  echo 'Error: docker compose is not installed.' >&2
   exit 1
 fi
 
@@ -29,15 +29,15 @@ fi
 echo "### Creating dummy certificate for $domains ..."
 path="/etc/letsencrypt/live/$domains"
 mkdir -p "$data_path/conf/live/$domains"
-docker-compose run --rm --entrypoint "openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 -keyout '$path/privkey.pem' -out '$path/fullchain.pem' -subj '/CN=localhost'" certbot
+docker compose run --rm --entrypoint "openssl req -x509 -nodes -newkey rsa:$rsa_key_size -days 1 -keyout '$path/privkey.pem' -out '$path/fullchain.pem' -subj '/CN=localhost'" certbot
 echo
 
 echo "### Starting nginx ..."
-docker-compose up -d nginx
+docker compose up -d nginx
 echo
 
 echo "### Deleting dummy certificate for $domains ..."
-docker-compose run --rm --entrypoint "rm -rf /etc/letsencrypt/live/$domains && rm -rf /etc/letsencrypt/archive/$domains && rm -rf /etc/letsencrypt/renewal/$domains.conf" certbot
+docker compose run --rm --entrypoint "rm -rf /etc/letsencrypt/live/$domains && rm -rf /etc/letsencrypt/archive/$domains && rm -rf /etc/letsencrypt/renewal/$domains.conf" certbot
 echo
 
 echo "### Requesting Let's Encrypt certificate for $domains ..."
@@ -56,8 +56,8 @@ esac
 # Enable staging mode if needed
 if [ $staging != "0" ]; then staging_arg="--staging"; fi
 
-docker-compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot $staging_arg $email_arg $domain_args --rsa-key-size $rsa_key_size --agree-tos --force-renewal" certbot
+docker compose run --rm --entrypoint "certbot certonly --webroot -w /var/www/certbot $staging_arg $email_arg $domain_args --rsa-key-size $rsa_key_size --agree-tos --force-renewal" certbot
 echo
 
 echo "### Reloading nginx ..."
-docker-compose exec nginx nginx -s reload
+docker compose exec nginx nginx -s reload
