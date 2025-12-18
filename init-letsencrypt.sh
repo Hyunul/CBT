1164b9fd7579a53f20021eaed537bb34745e8f2b
hyunul <jungwj1023@naver.com>
2025-12-18 23:51:47 +0900

    fix: ssl script

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -5,11 +5,11 @@ if ! [ -x "$(command -v docker-compose)" ]; then
   exit 1
 fi
 
-domains=(cbt.example.com)
+domains=(www.hyunul.shop)
 rsa_key_size=4096
 data_path="./certbot"
-email="" # Adding a valid address is strongly recommended
-staging=0 # Set to 1 if you're testing your setup to avoid hitting request limits
+email="" # 알림을 받으려면 여기에 이메일을 입력하세요 (예: user@example.com)
+staging=0 # 테스트 시 1, 실제 발급 시 0
 
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
